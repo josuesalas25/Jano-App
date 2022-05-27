94d1d2b32d55d9aeb2350159f7dbadd04d725892
josuesalas25 <39318241+josuesalas25@users.noreply.github.com>
2022-05-27 13:54:53 +0200

    update

--- JanoApp/JanoApp/settings.py
+++ JanoApp/JanoApp/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = BASE_DIR / 'docs'
+TEMPLATE_DIR = BASE_DIR / 'templates'
 STATIC_DIR = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
